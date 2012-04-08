b66fd4276873acaab42e0565e900fe157a653f09
Erik Landvall <erik@landvall.se>
2012-04-08 18:47:39 +0200

    Translated the documentation and made a few updates to the code

--- animasies.js.js
+++ animasies.js.js
@@ -1,5 +1,5 @@
 /*
-    Animasies - Javascript animation pack
+    Animasies - Javascript - Animation calculations
     Copyright (C) 2011  Erik Landvall
     
     Animasies is free software: you can redistribute it and/or modify
@@ -16,336 +16,303 @@
     along with this program.  If not, see http://www.gnu.org/licenses/.
  */
 
-/**
- * Klassen håller statiska funktioner för animeringar
+/* Cumulativa sum
+ * 
+ * @param vector array The vector that the end resoult should be based upon.
+ * 
+ * @return Array
+ */
+Math.cumsum = Math.cumsum || function( vektor )
+{
+    var cumsum  = [];
+    cumsum[ 0 ] = vektor[ 0 ];
+    for ( var i = 1; i < vektor.length; i++ )
+        cumsum[ i ] = cumsum[ i-1 ] + vektor[ i ];
+
+    return cumsum;
+}
+
+/* This class containes static funktions that can be used in animations
  * 
  * @class
  * @static
  */
 var Animasies =
 {
-    /**
-     * <u>Gaussian curve / General normal distribution curve / Bell curve /
-     * Normalfördelningskurvan</u><br />
-     * <br />
-     * Funktionen returnerar ett fält med jämt fördelade integers som motsvarar,
-     * steg för steg, en animation enligt den så kallade "klock kurvan".<br />
-     * <br />
-     * Olika variablar som används i funktionen och deras beskrivningar följer
-     * här:<br />
-     * <br />
-     * <b>start</b> = var animationen ska starta.<br />
-     * <b>length</b> = Längden. Målet.<br />
-     * <b>position</b> = position av kurvan, maxhastighetens position, ifall den
-     * ska sluta mer tvärt eller börja snabbare. 0.5 = mitten. variablen ska
-     * vara ett decimaltal mellan 0 - 1<br />
-     * <b>h</b> = Steglängden.<br />
-     * <b>s</b> = standardavvikelsen (bredden på intervallet)<br />
-     * Standard avikelsen räknas ut genom halva längden delat med tre:
-     * length/2/3 = length/6<br />
-     * <b>j</b> = maxhastighetensläge<br />
-     * <b>y</b> = normalfördelningskurvan<br />
-     * <b>z</b> = cumulativa summan av var:y<br />
-     * <b>v</b> = procentuell anpassningsfaktor, så att sista posten i var:u får
-     * cumsum = 1<br />
-     * <b>u</b> = det nya anpassade fältet med lägeskordinaterna<br />
-     * @param length int<br />
-     * Längden på animationen
-     * @param position float<br />
-     * Ett decimaltal mellan 0 - 1 som avgör var kurvan ska ha sin topp.
-     * Mitten = 0.5
+    /* The Normal distribution curve, Gaussian curve,
+     * General normal distribution curve, Bell curve
+     * 
+     * @param length int The length of the animation
+     * 
+     * @param position float [optional] A decimal number between 0 - 1 that
+     * tells where the curve should have its peak, where the speed is the
+     * highest. The middle is 0.5. Default value set to 0.5
      *
      * @function
      * @static
      * @return Array
      * @type Array
      */
-    bell : function(length,position)
+    bell : function( length, position )
     {
-        var h       = length/(((180/3395)*length)+84.09425626),
-            s       = length/10,
-            j       = length*position,
-            x       = [],
-            y       = [],
+        position = position || 0.5;
+        
+        var h   = length / (( 0.053019146 * length ) + 84.09425626 ),
+            s   = length / 10,
+            j   = length * position,
+            x   = [],
+            y   = [],
             z,
             v,
-            u       = [],
-            temp    = length - ( length % h );
+            u   = [],
+            tmp = length - ( length % h );
             
-        for(var n = 0, i = 0; n < temp; n += h, i++)
-            x[i] = n;
-        x[i] = length;
+        for( var n = 0, i = 0; n < tmp; n += h, i++ )
+            x[ i ] = n;
+        x[ i ] = length;
+        
+        tmp = h * ( 1 / ( s * ( Math.sqrt( 2 * Math.PI ))));
+        for( i = 0; i < x.lenth; i++ )
+            y.push( 
+                tmp 
+                * Math.exp(
+                    ( Math.pow( x[ i ] - j, 2 ) 
+                    / Math.pow( 2 * s, 2 )
+                    ) * -1 ));
 
-        // Temporär konstant för att slippa göra samma uträkning gång på gång
-        temp = h*(1/(s*(Math.sqrt(2*Math.PI))));
-        for(n in x)
-            // Normalfördelningen räknas ut och sparas i fältet y
-            y[n] = temp * Math.exp(-(Math.pow(x[n]-j,2)/Math.pow(2*s,2)));
+        z = Math.cumsum( y );
+        v = length / z[ z.length - 1 ];
 
-        z = Math.cumsum(y);
-        v = length/z[z.length-1];
-
-        for(n in z)
-            u[n] = Math.round(v * z[n]);
+        for( i = 0; i < z.lenth; i++ )
+            u.push( Math.round( v * z[ i ] ));
+        
+        if( u.length > 0 )
+            u[ u.length - 1 ] = length;
 
         return u;
     },
 
-    /**
-     * Funktionen är till för att animera ett element med en dämpad
-     * svängningsrörelse…<br />
-     * <br />
-     * <u>De parametrar som finns:</u><br />
-     * <b>yv</b> integer <br />
-     * Ny Y-axel sparad i ett fält, måste avrundas för gämnare slut.<br />
-     * <br />
-     * <b>r</b> integer <br />
-     * Uträkningens resultat för att slippa göra samma uträkning 2 gånger.<br />
-     * <br />
-     * <b>element</b> Element <br />
-     * Det element som ska animeras. Variabeln är en parameter<br />
-     * <br />
-     * <b>t</b> integer <br />
-     * Tid/Klocka / (acceleration).<br />
-     * <br />
-     * <b>a</b> integer <br />
-     * Initsial amplitude / Storlek på första svängningen / (var objektet släpps
-     * ifrån).<br />
-     * <br />
-     * <b>d</b> integer <br />
-     * Dämpningen.<br />
-     * <br />
-     * <b>f</b> integer <br />
-     * Frekvens / naturlig svängning / (antal svängningar).<br />
-     *
-     * @param a int<br />
-     * Beskriver var objektet släpps ifrån med pixlar som måttstock.
-     * @param d int<br />
-     * Dämpningen som oscillationen har. Defaultvärdet är -0.7.<br />
-     * <b>OBS!</b> Måste vara ett negativt värde
+    /* The functionen returns an array of the damped oscillation movement
+     * 
+     * @param height int The initial amplitude, describes from what height the
+     * object is droped from.
+     * 
+     * @param attenuation float [optional] The attenuation that the
+     * oscillationen will have. Default value set to 0.7
+     * 
+     * @param frequency float [optional] The natural oscillation, how light the
+     * oscillation should be. Default value set to 1.8
      *
      * @function
      * @static
      * @return Array
      * @type Array
      */
-    dampedOscillation : function(a,d)
+    dampedOscillation : function( height, attenuation, frequency )
     {
-        d  = d || -0.7;
+        attenuation = attenuation || 0.7;
+        attenuation = Math.abs( attenuation ) * -1;
+        
+        frequency   = frequency || 1.8;
+        frequency   = Math.abs( frequency );
+        
         var t  = 0,
-            f  = 1.8,
             yv = [],
-            r,
-            i  = 0,
-            setArray = function()
-            {
-                t      += 0.1;
-                r       = a*Math.pow(Math.E,(d*t))*Math.cos((f*t)+Math.PI);
-                yv[i++] = Math.round(r);
-            };
+            r;
 
-        do setArray();
-        while(Math.abs(r) >= 0.005);
+        do 
+        {
+            t += 0.1;
+            r  = height
+               * Math.pow( 
+                    Math.E,
+                    attenuation * t )
+               * Math.cos(( frequency * t ) + Math.PI );
+                
+            yv.push( Math.round( r ));
+        }
+        while( Math.abs( r ) >= 0.005 );
 
         return yv;
     },
 
-    /**
-     * En aniamtion som påminner om när man stänger en rullgardin. Åker först
-     * ner lite innan den faller snabbare uppåt.
-     * @param distance int<br />
-     * Distansen som animationen ska röra sig på
+    /* Basic jump and fall movement 
+     * 
+     * @param distance int The distance we wish to animate
      *
      * @function
      * @static
      * @return Array
      * @type Array
      */
-    curtainClose : function(distance)
+    curtainClose : function( distance )
     {
-        var value       = 0,
-            t           = 0,
-            pull        = 0,
-            aniArray    = [];
+        var value = 0,
+            t     = 0,
+            pull  = 0,
+            ani   = [];
 
-        while(value < distance)
+        while( value < distance )
         {
-            t += 0.8;
+            t    += 0.8;
             pull += 10;
-            value = t*t-pull;
-            aniArray[aniArray.length] = Math.round(value);
+            value = t * t - pull;
+            ani.push( Math.round( value ));
         }
-        return aniArray;
+        
+        if( ani.length > 0 )
+            ani[ ani.length - 1 ] = distance;
+        
+        return ani;
     },
 
-    /**
-     * Funktionen returnerar en array som motsvarar en animation som liknar
-     * att något studsar.<br />
-     * <br />
-     * <u>De olika variablarna som används i funktionen:</u><br />
-     * <ul>
-     *  <li>
-     *      <b>y</b>    Dynamisk position av elementet
-     *  </li>
-     *  <li>
-     *      <b>v</b>    Dynamisk hastighet
-     *  </li>
-     *  <li>
-     *      <b>iH</b>   Constant , initsial höjd. Var elementet släpps ifrån
-     *  </li>
-     *  <li>
-     *      <b>y0</b>   Dynamisk , Var elementet släpps ifrån, initsial
-     *      höjd.
-     *  </li>
-     *  <li>
-     *      <b>v0</b>   Hastigheten vid t=0, (utgångshastigheten). Possetivt
-     *      tal säger att man kastar den uppot, samt ett negativt tal säger
-     *      att man kastar den neråt. +-0 = att man bara släpper den helt
-     *      enkelt.
-     *  </li>
-     *  <li>
-     *      <b>g</b>    Tyngdkraften, dragningskraften. Default värde 9,81.
-     *  </li>
-     *  <li>
-     *      <b>n</b>    Den procent av kraften i decimaltal som finns kvar
-     *      då den ska studsa. 1.0 = 100%, det vill säga en gämn studs.
-     *  </li>
-     *  <li>
-     *      <b>s</b>    Constant, antalet studsar som ska utföras.
-     *  </li>
-     *  <li>
-     *      <b>t</b>    Tiden, ticka
-     *  </li>
-     *  <li>
-     *      <b>data</b> Uträkningen lagras i en vektor
-     *  </li>
-     * </ul>
+    /* A bouncing movement
      *
-     * @example
-     * var i = 0,
-     *     data = C.Animation.bounce(),
-     *     animation = function(){
-     *          if(i < data.length){
-     *              target.style.top = data[i++] + "px";
-     *              setTimeout(animation,15);
-     *          }
-     *      };
-     *  animation();
+     * @param height int Initial height. Where the element is beeing droped
+     * from.
      *
-     * @param iH int<br />
-     * Initsialhöjden. Var elementet släpps ifrån.
-     *
+     * @param gravity float [optional] The gravity. Default value set to 125.
+     * 
+     * @param remainingForce float [optional] The force that will push the
+     * element up when the element hits the bottom bounce expressed in percent.
+     * Default value set to 0.6.
+     * 
+     * @param max int [optional] The maximum amount of bounces that can accure
+     * before the loop ends. Default value set to 15.
+     * 
      * @function
      * @static
      * @return Array
      * @type Array
      */
-    bounce : function(iH)
+    bounce : function( height, gravity, remainingForce, max )
     {
-        var y       = 0,
-            v       = 0,
-            y0      = iH,
-            v0      = 0,
-            g       = 125,
-            n       = 0.6,
-            s       = 15,
-            t       = 0,
+        gravity = gravity || 125;
+        gravity = Math.abs( gravity );
+        
+        remainingForce = remainingForce || 0.6;
+        remainingForce = Math.abs( remainingForce );
+        
+        max = max || 15;
+        max = Math.abs( max );
+        
+        var y    = 0,
+            v    = 0,
+            y0   = height,
+            v0   = 0,
+            t    = 0,
             fv0,
-            data    = [];
+            data = [];
 
-        for(var m = 0; m < s; m++)
+        for( var i = 0; i < max; i++ )
         {
             fv0 = v0;
-            while(y >= 0)
+            while( y >= 0 )
             {
-                y  = y0 + (v0 * t) - (0.5 * g * Math.pow(t,2));
-                y  = Math.round(y);
-                v  = v0 - (g*t);
+                y  = y0 + ( v0 * t ) - ( 0.5 * gravity * Math.pow( t, 2 ));
+                y  = Math.round( y );
+                v  = v0 - ( gravity * t );
                 t += 0.1;
-                data[data.length] = Math.round(iH - (y < 0 ? 0 : y));
+                data.push( Math.round( height - ( y < 0 ? 0 : y )));
             }
-            // Återställer värden inför nästa studs
+            
+            // Reset before next bounce
             y  = 0;
             t  = 0;
             y0 = 0;
-            v0 = -v * n;
+            v0 = -v * remainingForce;
 
-            if(m > 0 && fv0 < v0)
+            if( i > 0 && fv0 < v0 )
                 break;
         }
+        
+        if( data.length > 0 )
+            data[ data.length - 1 ] = height;
 
         return data;
     },
 
-    /**
-     * Funktionen returnerar en array med fallande liknelse.<br />
-     * Rörelsen ökar ju längre den fortgår..
-     * @type array
-     * @return array
-     * @static
+    /* Basic falling movement
+     * 
+     * @param distance int The distance that the element should fall
+     * 
      * @function
-     * @param distance int<br />
-     * Den distansen som elementet ska falla
+     * @static
+     * @return Array
+     * @type Array
      */
-    fall : function(distance)
+    fall : function( distance )
     {
-        var value       = 0,
-            t           = 0,
-            aniArray    = [];
+        var value = 0,
+            t     = 0,
+            ani   = [];
 
-        while(value < distance)
+        while( value < distance )
         {
-            t += 0.8;
-            value = t*t;
-            aniArray[aniArray.length] = Math.round(value);
+            t    += 0.8;
+            value = t * t;
+            
+            ani.push( Math.round( value ));
         }
-        return aniArray;
+        
+        if( ani.length > 0 )
+            ani[ ani.length - 1 ] = distance;
+        
+        return ani;
     },
 
-    /**
-     * Funktionen returnerar en array med en tråkig konstant rörelse.<br />
-     * @type array
-     * @return array
-     * @static
+    /* Constant motion
+     * 
+     * @param distance int The distance that the element should move
+     * 
+     * @param speed int [optional] The speed of the movement. Default value set
+     * to 1.
+     * 
      * @function
-     * @param distance int<br />
-     * Den distansen som elementet ska röra sig
-     * @param speed int<br />
-     * Hastigheten på rörelsen..<br />
-     * Default = 1
+     * @static
+     * @return Array
+     * @type Array
      */
-    constantMotion : function(distance,speed)
+    constantMotion : function( distance, speed )
     {
         speed = speed || 1;
+        speed = Match.abs( speed );
 
-        var value       = 0,
-            aniArray    = [];
+        var value = 0,
+            ani   = [];
 
-        while(value < distance)
+        while( value < distance )
         {
             value += speed;
-            aniArray[aniArray.length] = Math.round(value);
+            ani.push( Math.round( value ));
         }
-        return aniArray;
+        
+        if( ani.length > 0 )
+            ani[ ani.length - 1 ] = distance;
+        
+        return ani;
     },
     
-    /**
-     * Half of a sinus curve, returns
+    /* Half of a sinus curve, returns
      * 
-     * @type Array
-     * @return Array
-     * @static
-     * @function
      * @param distance int The distance we wish to animate
-     * @param speed double The speed of the movement, default set to 0.1
+     * 
+     * @param speed [optional] int The speed of the movement. Default value set
+     * to 1.
+     * 
+     * @function
+     * @static
+     * @return Array
+     * @type Array
      */
     pie : function( distance, speed )
     {
-        speed = speed || 0.1;
+        speed  = speed || 1;
+        speed /= 10;
 
-        var animation = [],
-            former    = 0,
-            ticker    = 0,
+        var ani    = [],
+            former = 0,
+            ticker = 0,
             value;
 
         while( true )
@@ -358,32 +325,35 @@ var Animasies =
 
             former = value;
 
-            animation.push( Math.round( value * distance ));
+            ani.push( Math.round( value * distance ));
         }
 
-        if( animation.length > 0 )
-            animation[ animation.length - 1 ] = distance;
+        if( ani.length > 0 )
+            ani[ ani.length - 1 ] = distance;
 
-        return animation;
+        return ani;
     },
     
-    /**
-     * A positive sinus curve
+    /* A positive sinus curve
      * 
-     * @type Array
-     * @return Array
-     * @static
-     * @function
      * @param distance int The distance we wish to animate
-     * @param speed double The speed of the movement, default set to 0.1
+     * 
+     * @param speed int [optional] The speed of the movement. Default value set
+     * to 1.
+     * 
+     * @function
+     * @static
+     * @return Array
+     * @type Array
      */
     halfMoon : function( distance, speed )
     {
-        speed = speed || 0.1;
+        speed  = speed || 1;
+        speed /= 10;
 
-        var animation = [],
-            former    = 0,
-            ticker    = ( Math.PI / 2 ) * - 1,
+        var ani    = [],
+            former = 0,
+            ticker = ( Math.PI / 2 ) * - 1,
             value;
 
         while( true )
@@ -396,12 +366,12 @@ var Animasies =
 
             former = value;
 
-            animation.push( Math.round( value * distance ));
+            ani.push( Math.round( value * distance ));
         }
 
-        if( animation.length > 0 )
-            animation[ animation.length - 1 ] = distance;
+        if( ani.length > 0 )
+            ani[ ani.length - 1 ] = distance;
 
-        return animation;
+        return ani;
     }
 }
